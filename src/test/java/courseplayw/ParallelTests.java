929b90a99b581236a0145eb0ae385315092b5d11
Полина Матвеева <polinachetc@gmail.com>
2026-02-24 21:42:16 +0300

    Практическое задание 6

---
+++ src/test/java/courseplayw/ParallelTests.java
@@ -0,0 +1,43 @@
+package courseplayw;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParallelTests {
+
+    @Test
+    public void testLoginPage() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false)
+            );
+            try (BrowserContext context = browser.newContext()) {
+                Page page = context.newPage();
+                page.navigate("https://the-internet.herokuapp.com/login");
+                assertEquals("The Internet", page.title());
+            }
+            browser.close();
+        }
+    }
+
+    @Test
+    public void testAddRemoveElements() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false)
+            );
+            try (BrowserContext context = browser.newContext()) {
+                Page page = context.newPage();
+                page.navigate("https://the-internet.herokuapp.com/add_remove_elements/");
+                page.click("button:text('Add Element')");
+                assertTrue(page.isVisible("button.added-manually"));
+                page.click("button.added-manually");
+                assertFalse(page.isVisible("button.added-manually"),
+                        "Кнопка должна исчезнуть после удаления");
+            }
+            browser.close();
+        }
+    }
+}
